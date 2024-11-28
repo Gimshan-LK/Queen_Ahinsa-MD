75be817b223b6be1f133f65cbfa3320d94576da5
Koyeb-LK <dilishadilisha29@gmail.com>
2024-11-28 20:40:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,21 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Fu93DSAS#REOYweYsbhX5Jwa2X5vrht_WQpKrReGGeZEg5olWZJQ",
-MONGODB: process.env.MONGODB || "mongodb+srv://whatsappbot11000:<db_password>@cluster0.hbfdf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-
+SESSION_ID: process.env.SESSION_ID || "enter your session",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/SR76mBh/Pu3-ZYHBS5139.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HII FRIEND I AM ONLINE NOW 👋",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+HEART_REACT: process.env.HEART_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "true",
+BOT_NAME: process.env.BOT_NAME || "QUEEN-AHINSA-MD",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
