3bd250f18d41dac26d2c6e756e843dd2c523a066
Koyeb-LK <dilishadilisha29@gmail.com>
2024-11-30 08:21:47 +0530

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -43,7 +43,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
  *Enjoy these images! 👾*
 
-> 🌈*SILENT-SOBX-MD BOT PLUGINS*🏝️`
+> 🌈*QUEEN_AHINSA-MD BOT PLUGINS*🏝️`
 }, { quoted: mek });
 }
 
