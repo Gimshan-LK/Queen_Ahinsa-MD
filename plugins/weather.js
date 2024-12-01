a69ae9d0c218569bdbb681e7a27be671d96221b6
Koyeb-LK <dilishadilisha29@gmail.com>
2024-12-01 15:15:30 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const config = require('../config');
 const { cmd, commands } = require('../command');
+
 cmd({
     pattern: "weather",
     desc: "🌤 Get weather information for a location",
@@ -11,13 +12,17 @@ cmd({
 async (conn, mek, m, { from, q, reply }) => {
     try {
         if (!q) return reply("❗ Please provide a city name. Usage: .weather [city name]");
+
         const apiKey = '2d61a72574c11c4f36173b627f8cb177'; 
         const city = q;
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
         const response = await axios.get(url);
         const data = response.data;
+
         const weather = `
 🌍 *Weather Information for ${data.name}, ${data.sys.country}* 🌍
+
 🌡️ *Temperature*: ${data.main.temp}°C
 🌡️ *Feels Like*: ${data.main.feels_like}°C
 🌡️ *Min Temp*: ${data.main.temp_min}°C
@@ -28,8 +33,9 @@ async (conn, mek, m, { from, q, reply }) => {
 💨 *Wind Speed*: ${data.wind.speed} m/s
 🔽 *Pressure*: ${data.main.pressure} hPa
 
-*POWERED BY SILENTLOVER432*
+*POWERED by Queen_Ahinsa-MD*
 `;
+
         return reply(weather);
     } catch (e) {
         console.log(e);
@@ -39,3 +45,4 @@ async (conn, mek, m, { from, q, reply }) => {
         return reply("⚠️ An error occurred while fetching the weather information. Please try again later.");
     }
 });
+
